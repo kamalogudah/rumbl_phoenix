b193721cf69dd759f9a331bc7acccc2a721b1cdd
kamalogudah <mcpaul2058@gmail.com>
2019-08-04 18:59:33 +0300

    Building the Repository to Add Data

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
+# Use Jason for JSON parsing in Phoenixp
 config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,22 +3,23 @@ defmodule Rumbl.Accounts do
   The accounts context.
   """
 
+  alias Rumbl.Repo
+
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Jose Valim", username: "josevalim"},
-      %User{id: "2", name: "Paul Oguda", username: "kamalogudah"},
-      %User{id: "3", name: "Mike Opinde", username: "mikey"}
-  ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-     Enum.find(list_users(), fn map -> map.id == id end)
+     Repo.get(User, id)
   end
 
+  def get_user!(id) do
+    Repo.get!(User, id)
+ end
+
   def get_user_by(params) do
-    Enum.find(list_users(), fn map -> Enum.all?(params, fn {key, val} ->
-      Map.get(map, key) == val end) end)
+    Repo.get_by(User, params)
   end
 end
