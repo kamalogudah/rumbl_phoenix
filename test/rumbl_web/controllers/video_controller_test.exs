6d9126773e0d1a6b49a173f1b5f1af7f9dfb260c
kamalogudah <mcpaul2058@gmail.com>
2019-08-19 15:56:35 +0300

    Testing Logged in users

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -1,5 +1,7 @@
 defmodule Rumbl.VideoControllerTest do
   use RumblWeb.ConnCase, async: true
+  
+  
 
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each(
@@ -18,4 +20,24 @@ defmodule Rumbl.VideoControllerTest do
       end
     )
   end
+  
+  describe "Logged in user" do
+  
+  setup %{conn: conn, login_as: username} do
+    user = user_fixture(username: username)
+    conn = assign(conn, :current_user, user)
+    
+    {:ok, conn: conn, user: user}
+  end
+  test "list all user's videos on index", %{conn: conn, user: user} do
+    user_video = video_fixture(user, title: "funny cats")
+    other_video = video_fixture(user_fixture(username: "other"), title: "another video")
+    conn = get conn, Routes.video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing Videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    assert String.contains?(conn.resp_body, other_video.title)
+
+    
+  end
+end
 end
